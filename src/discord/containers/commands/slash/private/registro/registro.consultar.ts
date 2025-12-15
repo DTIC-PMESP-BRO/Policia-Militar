3fd1b45145cb52241332dd31875eb46be2c893ab
YuriGRG <134341981+YuriGRG@users.noreply.github.com>
2025-12-15 18:12:48 -0300

    feat: Sistema de Registro de Militares

---
+++ src/discord/containers/commands/slash/private/registro/registro.consultar.ts
@@ -0,0 +1,26 @@
+import { brBuilder, createContainer, createSection } from "@magicyan/discord";
+import { GuildMember } from "discord.js";
+import { icon } from "../../../../../../functions/utils/emojis.js";
+
+export function registroConsultarContainer(member: GuildMember, nome: string, patente: string, rg: string, opm: string, status: string) {
+    return createContainer({
+        components: [
+            createSection(
+                brBuilder(
+                    `## ${icon.user} ${nome}`,
+                    `-# Patente: ${patente}`,
+                    `-# Status: ${status}`,
+                    `-# RG: ${rg}`,
+                    `-# OPM: ${opm}`
+                ),
+                {
+                    media: {
+                        url: member.displayAvatarURL()
+                    },
+                    description: `Perfil de ${member.user.username}`
+                }
+            )
+        ]
+    })
+}
+
--- src/functions/utils/createRegistroDocument.ts
+++ src/functions/utils/createRegistroDocument.ts
@@ -1,13 +1,14 @@
 import { db } from "../../database/firestore.js";
 
-export async function createRegistroDocument(memberId: string, nome: string, rg: string, patente:string, opm: string): Promise<void> {
+export async function createRegistroDocument(memberId: string, nome: string, rg: string, patente:string, opm: string, status: string): Promise<void> {
     try {
-        await db.collection("militares").doc(memberId).set({
+        await db.collection("militares").doc(rg).set({
             memberId: memberId,
             nome: nome,
             rg: rg,
             patente: patente,
-            opm: opm
+            opm: opm,
+            status: status
         });
         console.log("Documento de militar criado com sucesso!");
     } catch (error) {
